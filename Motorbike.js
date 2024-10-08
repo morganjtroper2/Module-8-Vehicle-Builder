ff4e3c486fe22fc7b4030df41f983bd18ac1b5e5
morganjtroper2 <morganjtroper@gmail.com>
2024-10-07 20:22:24 -0700

    Add files from local
    Contains .ts files and post-compilation .js files

---
+++ Motorbike.js
@@ -0,0 +1,35 @@
+import Vehicle from './Vehicle.js';
+import Wheel from './Wheel.js';
+class Motorbike extends Vehicle {
+    constructor(vin, color, make, model, year, weight, topSpeed, wheels) {
+        super();
+        this.vin = vin;
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.weight = weight;
+        this.topSpeed = topSpeed;
+        if (wheels.length !== 2) {
+            this.wheels = [new Wheel(), new Wheel()];
+        }
+        else {
+            this.wheels = wheels;
+        }
+    }
+    wheelie() {
+        console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+    }
+    printDetails() {
+        super.printDetails();
+        console.log(`VIN: ${this.vin}`);
+        console.log(`Color: ${this.color}`);
+        console.log(`Make: ${this.make}`);
+        console.log(`Model: ${this.model}`);
+        console.log(`Year: ${this.year}`);
+        console.log(`Weight: ${this.weight} lbs`);
+        console.log(`Top Speed: ${this.topSpeed} mph`);
+        console.log(`Wheels: ${this.wheels}`);
+    }
+}
+export default Motorbike;
